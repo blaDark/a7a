bfc2df54044596a72a4cb1567b90b6bd252989b1
Glitch (giveaway2) <none>
2020-01-26 18:35:28 +0000

    🚥🤗 Checkpoint ./server.js:4981465/316

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://Name-Project.glitch.me/`);
+}, 280000);
+
+//////////////////
+
 const { Client, RichEmbed } = require('discord.js');
 const client = new Client({ disableEveryone: true });
 const Enmap = require('enmap');
